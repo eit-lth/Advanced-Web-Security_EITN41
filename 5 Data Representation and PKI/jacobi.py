6d842754b31882cf1d826e249a70359698cec976
Martin Hell <martin.hell@gmail.com>
2017-05-18 13:16:00 +0200

    PDFs and code
    Two PDFs added together with Jacobi code in java and paython

---
+++ 5 Data Representation and PKI/jacobi.py
@@ -0,0 +1,14 @@
+def jacobi (a, m):
+	j = 1
+	a %= m
+	while a
+		t = 0
+		while not a & 1:
+			a = a >> 1
+			t += 1
+		if t&1 and m%8 in (3, 5):
+			j = -j
+		if (a % 4 == m % 4 == 3):
+			j = -j
+		a, m = m % a, a
+	return j if m == 1 else 0
