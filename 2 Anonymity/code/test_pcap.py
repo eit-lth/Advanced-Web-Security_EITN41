6d63221c91c3d07dd8940c6a9d9a728de1d2a10c
Jonathan Sönnerup <Nooxet@gmail.com>
2017-11-08 10:54:32 +0100

    updated test file for parsing pcap in python

--- 2 Anonymity/code/test_pcap.py
+++ 2 Anonymity/code/test_pcap.py
@@ -1,10 +1,18 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
 
-from scapy.all import *
+from pcapfile import savefile
 
-pkts = rdpcap('test.pcap')
+testcap = open('test.pcap', 'rb')
+capfile = savefile.load_savefile(testcap, layers=2, verbose=True)
 
-print ('timestamp\tEth src\t\t\tEth dst\t\t\tIP src\t\tIP dst')
-for pkt in pkts:
-    print ('{}\t\t{}\t{}\t{}\t{}'.format(pkt.time, pkt.getlayer(Ether).src, pkt.getlayer(Ether).dst, 
-        pkt.getlayer(IP).src, pkt.getlayer(IP).dst))
+# print the packets
+print ('timestamp\teth src\t\t\teth dst\t\t\tIP src\t\tIP dst')
+for pkt in capfile.packets:
+    timestamp = pkt.timestamp
+    # all data is ASCII encoded (byte arrays). If we want to compare with strings
+    # we need to decode the byte arrays into UTF8 coded strings
+    eth_src = pkt.packet.src.decode('UTF8')
+    eth_dst = pkt.packet.dst.decode('UTF8')
+    ip_src = pkt.packet.payload.src.decode('UTF8')
+    ip_dst = pkt.packet.payload.dst.decode('UTF8')
+    print ('{}\t\t{}\t{}\t{}\t{}'.format(timestamp, eth_src, eth_dst, ip_src, ip_dst))
