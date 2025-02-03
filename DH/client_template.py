36ed0224cf830320e3e9a907547677650f687dad
OChicken <shouran.ma@gmail.com>
2025-02-03 21:28:48 +0100

    choose local or remote to connect

--- DH/client_template.py
+++ DH/client_template.py
@@ -38,7 +38,9 @@ if __name__ == '__main__':
     p = int('ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff', 16)
     # the generator of the group (Z/pZ)*
     g = 2
+    # connect
+    host = "0.0.0.0"
+    host = "igor.eit.lth.se"
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # client.connect(("0.0.0.0", 6004))
-    client.connect(("igor.eit.lth.se", 6004))
+    client.connect((host, 6004))
     # Add your own code for DH kex
